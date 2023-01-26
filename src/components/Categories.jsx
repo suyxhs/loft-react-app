8f735e4d7c47fa4da923a8dc6296290b12e69c11
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-01-26 22:17:39 +0300

    Create Categories.jsx

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Двери</li>
+        <li>Стулья</li>
+        <li>Столы</li>
+        <li>Шкафы</li>
+        <li>Светильники</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
