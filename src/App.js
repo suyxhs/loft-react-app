00f56339be8437df9f651d37156757fecb4ad68f
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-02 02:34:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ import Categories from "./components/Categories";
 import Banner from "./components/Banner";
 import Sort from "./components/Sort";
 import LoftBlock from "./components/LoftBlock";
-
-import itemLoft from "./assets/itemLoft.json";
+import Footer from "./components/Footer";
+import Skeleton from "./components/LoftBlock/Skeleton";
 
 import "./scss/app.scss";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://63d83131baa0f79e09a56b4b.mockapi.io/items")
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -23,15 +35,19 @@ function App() {
           </div>
           <h2 className="content__title">Вся мебель</h2>
           <div className="content__items">
-            {itemLoft.map((obj) => (
-              <LoftBlock
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-              />
-            ))}
+            {isLoading
+              ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => (
+                  <LoftBlock
+                    key={obj.id}
+                    title={obj.title}
+                    price={obj.price}
+                    imageUrl={obj.imageUrl}
+                    sizes={obj.sizes}
+                  />
+                ))}
           </div>
+          <Footer />
         </div>
       </div>
     </div>
