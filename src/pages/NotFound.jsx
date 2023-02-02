a19b77a4f7ed500bf6296322b2bc9de4c0079650
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-03 01:30:03 +0300

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotFoundBLock from '../components/NotFoundBlock';
+
+const NotFound = () => {
+  return (
+    <NotFoundBLock />
+  )
+}
+
+export default NotFound;
