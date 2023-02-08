fc7fd5ec3bdfeae7e6af3b28e00ee3cf5deb016a
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-08 03:43:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
 import "./scss/app.scss";
-import {  Routes, Route } from "react-router-dom";
+
+export const SearchContext = React.createContext();
 
 function App() {
+  const [searchValue, setSearchValue] = React.useState("");
 
   return (
     <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </div>
-      </div>
+      </SearchContext.Provider>
     </div>
   );
 }
