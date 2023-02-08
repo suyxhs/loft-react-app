5c70b9f9b719e94d6fa0a7271268726df19b19e3
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-08 03:43:48 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,42 +5,69 @@ import Sort from "../components/Sort";
 import LoftBlock from "../components/LoftBlock";
 import Skeleton from "../components/LoftBlock/Skeleton";
 import Banner from "../components/Banner";
+import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
 
 const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const [sortType, setSortType] = React.useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   React.useEffect(() => {
-    fetch("https://63d83131baa0f79e09a56b4b.mockapi.io/items")
+    setIsLoading(true);
+
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
+
+    fetch(
+      `https://63d83131baa0f79e09a56b4b.mockapi.io/items?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-  }, []);
+    {
+      /*window.scrollTo(0, 0);*/
+    }
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const loftItems = items.map((obj) => (
+    <LoftBlock
+      key={obj.id}
+      title={obj.title}
+      price={obj.price}
+      imageUrl={obj.imageUrl}
+      sizes={obj.sizes}
+    />
+  ));
+
+  const skeletons = [...new Array(10)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
 
   return (
-    <>
+    <div className="container">
       <Banner />
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onClickCategory={(index) => setCategoryId(index)}
+        />
+        <Sort value={sortType} onClickSort={(index) => setSortType(index)} />
       </div>
-      <h2 className="content__title">Вся мебель</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => (
-              <LoftBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-              />
-            ))}
-      </div>
-    </>
+      <h2 className="content__title">Вся мебель:</h2>
+      <div className="content__items">{isLoading ? skeletons : loftItems}</div>
+      <Pagination onChangePage={number => setCurrentPage(number)} />
+    </div>
   );
 };
 
