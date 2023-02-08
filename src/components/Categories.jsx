dfbfb01e5505666dbfca25343373d4d5dde99879
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-08 03:43:27 +0300

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,31 +1,26 @@
 import React from "react";
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = React.useState(0);
+function Categories({value, onClickCategory}) {
 
   const categories = [
     "Все",
-    "Двери",
+    "Тумбочки",
     "Стулья",
     "Столы",
     "Шкафы",
-    "Светильники",
+    "Диваны",
   ];
-
-  const onClickCategory = (index) => {
-    setActiveIndex(index);
-  };
 
   return (
     <div className="categories">
       <ul>
-        {categories.map((value, index) => (
+        {categories.map((categoryName, index) => (
           <li
             key={index}
             onClick={() => onClickCategory(index)}
-            className={activeIndex === index ? "active" : ""}
+            className={value === index ? "active" : ""}
           >
-            {value}
+            {categoryName}
           </li>
         ))}
       </ul>
