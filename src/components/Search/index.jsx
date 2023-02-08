40c0fa497e89005b24e5e811fab0f2e7d7f41761
Alexander <104249649+suyxhs@users.noreply.github.com>
2023-02-08 03:43:40 +0300

    Create index.jsx

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,41 @@
+import React  from "react";
+import { SearchContext } from "../../App";
+
+import styles from "./Search.module.scss";
+
+const Search = () => {
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title />
+        <g data-name="Layer 2" id="Layer_2">
+          <path d="M18,10a8,8,0,1,0-3.1,6.31l6.4,6.4,1.41-1.41-6.4-6.4A8,8,0,0,0,18,10Zm-8,6a6,6,0,1,1,6-6A6,6,0,0,1,10,16Z" />
+        </g>
+      </svg>
+      <input
+        value={searchValue}
+        onChange={(event) => setSearchValue(event.target.value)}
+        className={styles.input}
+        placeholder="Поиск..."
+      />
+      {searchValue && (
+        <svg
+          onClick={() => setSearchValue('')}
+          className={styles.clearIcon}
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm1.41-1.41A8 8 0 1 0 15.66 4.34 8 8 0 0 0 4.34 15.66zm9.9-8.49L11.41 10l2.83 2.83-1.41 1.41L10 11.41l-2.83 2.83-1.41-1.41L8.59 10 5.76 7.17l1.41-1.41L10 8.59l2.83-2.83 1.41 1.41z" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
